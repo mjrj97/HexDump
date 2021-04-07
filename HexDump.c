1c09d8561894f3714c5d7aafaced4988bbb45012
mjrj97 <martin.jensen.1997@hotmail.com>
2021-04-07 23:25:19 +0200

    Simplified.

--- HexDump.c
+++ HexDump.c
@@ -2,28 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-const char* version = "1.1";
-
-// Reads the file a the specified location and size.
-unsigned char* ReadFile(const char* fileName, long fsize) {
-  FILE *f = fopen(fileName, "rb");
-  unsigned char *data = malloc(fsize + 1);
-  fread(data, 1, fsize, f);
-  fclose(f);
-
-  data[fsize] = 0;
-  return data;
-}
-
-// Returns the size of the file in bytes.
-long GetFileSize(const char* fileName) {
-  FILE *f = fopen(fileName, "rb");
-  fseek(f, 0, SEEK_END);
-  long fsize = ftell(f);
-  fseek(f, 0, SEEK_SET);
-  fclose(f);
-  return fsize;
-}
+const char* version = "1.2";
 
 // Returns 1 if the file exists and 0 if not.
 int FileExists(const char *fileName)
@@ -37,13 +16,6 @@ int FileExists(const char *fileName)
     return 0;
 }
 
-// Prints the version of the program.
-void PrintVersion() {
-  printf("Version: ");
-  printf(version);
-  printf("\n");
-}
-
 // Prints the possible command-line options.
 void PrintHelp() {
   printf("Command-line options:\n");
@@ -55,46 +27,45 @@ int main(int argc, char *argv[]) {
   if (argc > 1) {
     for (int i = 1; i < argc; i++) {
       if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--version") == 0){
-        PrintVersion();
+        printf("Version: %s\n", version);
       }
       else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0){
         PrintHelp();
       }
       else {
         if (FileExists(argv[i]) == 1) {
-          long fsize = GetFileSize(argv[1]);
-          unsigned char *data = ReadFile(argv[1], fsize);
+          FILE *f = fopen(argv[i], "rb");
+          fseek(f, 0, SEEK_END);
+          unsigned int fsize = ftell(f);
+          fseek(f, 0, SEEK_SET);
+          unsigned char *data = malloc(fsize + 1);
+          fread(data, 1, fsize, f);
+          fclose(f);
 
-          /*for (int i = 0; i < fsize; i++)
-            printf ("0x%x ", data[i]);*/
+          data[fsize] = 0;
 
           char ascii[17];
           ascii[16] = 0;
-          int rest = fsize%16 == 0 ? 0 : 16 - fsize%16;
+          unsigned int rest = fsize%16 == 0 ? 0 : 16 - fsize%16;
 
-          for (long i = 0; i < fsize + rest; i++) {
+          for (unsigned int i = 0; i < fsize + rest; i++) {
             if (i%16 == 0)
               printf("%08x ", i);
             if (i >= fsize) {
               ascii[i % 16] = ' ';
               printf ("   ");
             } else {
-			        ascii[i % 16] = data[i] >= ' ' && data[i] <= '~' ? data[i] : '.';
+              ascii[i % 16] = data[i] >= ' ' && data[i] <= '~' ? data[i] : '.';
               printf ("%02x ", data[i]);
-		        }
-            if (i%16 == 15)
-            {
-              printf(ascii);
-              printf("\n");
             }
+            if (i%16 == 15)
+              printf("%s\n", ascii);
           }
 
           free(data);
         }
         else {
-          printf("Couldnt' find file: ");
-          printf(argv[i]);
-          printf("\n");
+          printf("Couldnt' find file: %s\n", argv[i]);
         }
       }
     }
