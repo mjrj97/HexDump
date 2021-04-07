1088d60282c82e4625ece6c5f8f60af24c519352
mjrj97 <martin.jensen.1997@hotmail.com>
2021-04-07 23:49:22 +0200

    Optimized and refactorized.

--- HexDump.c
+++ HexDump.c
@@ -1,8 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-const char* version = "1.2";
+const char* version = "1.3";
+
+//Prints the data as a hex dump in console.
+void PrintDump(unsigned char *data, int fsize) {
+  char ascii[17];
+  ascii[16] = 0;
+  unsigned int i;
+
+  for (i = 0; i < fsize; i++) {
+    if (i%16 == 0)
+      printf("%08x ", i);
+    ascii[i % 16] = data[i] >= ' ' && data[i] <= '~' ? data[i] : '.';
+    printf ("%02x ", data[i]);
+    if (i%16 == 15)
+      printf("%s\n", ascii);
+  }
+  for (i = fsize%16; i < 16; i++) {
+    ascii[i % 16] = ' ';
+    printf ("   ");
+    if (i == 15)
+      printf("%s\n", ascii);
+  }
+}
+
+//Loads the bytes from a file.
+void LoadData(const char *fileName) {
+  FILE *f = fopen(fileName, "rb");
+  fseek(f, 0, SEEK_END);
+  unsigned int fsize = ftell(f);
+  fseek(f, 0, SEEK_SET);
+  unsigned char *data = malloc(fsize + 1);
+  fread(data, 1, fsize, f);
+  fclose(f);
+  data[fsize] = 0;
+
+  PrintDump(data, fsize);
+
+  free(data);
+}
 
 // Returns 1 if the file exists and 0 if not.
 int FileExists(const char *fileName)
@@ -24,6 +63,8 @@ void PrintHelp() {
 }
 
 int main(int argc, char *argv[]) {
+  clock_t begin = clock();
+
   if (argc > 1) {
     for (int i = 1; i < argc; i++) {
       if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--version") == 0){
@@ -34,35 +75,7 @@ int main(int argc, char *argv[]) {
       }
       else {
         if (FileExists(argv[i]) == 1) {
-          FILE *f = fopen(argv[i], "rb");
-          fseek(f, 0, SEEK_END);
-          unsigned int fsize = ftell(f);
-          fseek(f, 0, SEEK_SET);
-          unsigned char *data = malloc(fsize + 1);
-          fread(data, 1, fsize, f);
-          fclose(f);
-
-          data[fsize] = 0;
-
-          char ascii[17];
-          ascii[16] = 0;
-          unsigned int rest = fsize%16 == 0 ? 0 : 16 - fsize%16;
-
-          for (unsigned int i = 0; i < fsize + rest; i++) {
-            if (i%16 == 0)
-              printf("%08x ", i);
-            if (i >= fsize) {
-              ascii[i % 16] = ' ';
-              printf ("   ");
-            } else {
-              ascii[i % 16] = data[i] >= ' ' && data[i] <= '~' ? data[i] : '.';
-              printf ("%02x ", data[i]);
-            }
-            if (i%16 == 15)
-              printf("%s\n", ascii);
-          }
-
-          free(data);
+          LoadData(argv[i]);
         }
         else {
           printf("Couldnt' find file: %s\n", argv[i]);
@@ -73,6 +86,10 @@ int main(int argc, char *argv[]) {
   else {
     printf("No arguments found. Use -h or --help to list commands.\n");
   }
+
+  clock_t end = clock();
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("Time: %lf s", time_spent);
 
   return 0;
 }
